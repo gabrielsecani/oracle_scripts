abc0537c6763f576c31150c32367512c0baf957a
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-10-25 19:13:57 -0200

    bk.sh updated

--- backup/bk.sh
+++ backup/bk.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
-date
 echo "Executando $1"
+date
 
 setenv ORACLE_HOME /oracle/EP0/11204 
 setenv ORACLE_SID EP0
@@ -9,7 +9,14 @@ setenv ORACLE_BIN /oracle/EP0/11204/bin
 
 cd /backup
 
-$ORACLE_BIN/rman target / cmdfile=backup_$1.rman append log=backup.log 
-echo "FIM $1"
-date
+##rotate log
+setenv DATE_SUFFIX `date +"%Y"-"%m"-"%d"`
+setenv BKLOG "backup_$1_$DATE_SUFFIX.log"
 
+$ORACLE_BIN/rman target / cmdfile=backup_$1.rman append log=$BKLOG
+
+# Delete old log files.
+find backup*.log -mtime +30 -exec rm -f {} \;
+
+echo "Fim $1"
+date
