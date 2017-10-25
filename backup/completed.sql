2d7713d13b97215cc6269f59fc6232b55e27d597
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2017-10-25 19:13:29 -0200

    backup monitor & completed & growth scripts added

---
+++ backup/completed.sql
@@ -0,0 +1,15 @@
+﻿-- SCRIPT TO LIST COMPLETED RMAN BACKUP TIMINGS.
+
+col STATUS format a24
+col min format 99999.99
+col START_TIME for a20
+col END_TIME for a20
+set pages 99 linesize 235
+
+select SESSION_KEY, INPUT_TYPE, STATUS,
+       to_char(START_TIME,'dd/mm/yyyy hh24:mi') start_time,
+       to_char(END_TIME,'dd/mm/yyyy hh24:mi') end_time,
+       elapsed_seconds/60 min
+  from V$RMAN_BACKUP_JOB_DETAILS
+ order by session_key;
+/
---
+++ backup/monitor.sql
@@ -0,0 +1,12 @@
+﻿-- http://snapshottooold.blogspot.com.br/2017/09/oracle-script-to-monitor-rman-running.html?m=0
+col Timestarted format a16
+set linesize 160
+col opname for a18
+
+select sid,opname,trunc((totalwork*8192)/1024/1024) "Total work(MB)",
+ trunc((sofar*8192)/1024/1024) "Work Done(MB)", to_char(start_time, 'MM-DD-YYYY HH24:MI:SSAM') "Timestarted", trunc(time_remaining/60) "Time remaining(min)",round(elapsed_seconds/60) "Time Elasped(min)", round(sofar/totalwork*100,2) "% Complete" from v$session_longops
+ where opname like 'RMAN%'
+   and totalwork != 0
+   and sofar != totalwork
+ order by start_time, totalwork desc
+/
